1decc4afa56030648472714367276f9fec3a9972
Rocco Schulz <rocco.schulz@adesso.de>
2016-05-24 15:33:07 +0200

    add traffic incidents to simulation detail view.

--- modules/simulations/views/SimulationDetailView.js
+++ modules/simulations/views/SimulationDetailView.js
@@ -2,6 +2,8 @@ var Marionette = require('backbone.marionette');
 var tpl = require('../templates/simulationDetail.hbs');
 var TruckCollection = require('trucks/TruckCollection');
 var TruckListView = require('trucks/views/TruckListView');
+var TrafficCollection = require('traffic/TrafficCollection');
+var TrafficListView = require('traffic/views/TrafficListView');
 
 module.exports = Marionette.LayoutView.extend({
     template: tpl,
@@ -29,9 +31,21 @@ module.exports = Marionette.LayoutView.extend({
     },
 
     onShow: function() {
+        this._showTrucks();
+        this._showTraffic();
+    },
+
+    _showTrucks: function() {
         var trucks = new TruckCollection(this.model.get('_id'));
         var view = new TruckListView({collection: trucks});
         this.showChildView('trucks', view);
         trucks.fetch();
+    },
+
+    _showTraffic: function() {
+        var traffic = new TrafficCollection(this.model.get('_id'));
+        var view = new TrafficListView({collection: traffic});
+        this.showChildView('traffic', view);
+        traffic.fetch();
     }
 });
